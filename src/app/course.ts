fe9880bb72653e12a64ffd47580cd52e5e1aa417
yeoj1092 <yeoj1092@gmail.com>
2022-06-12 19:09:42 +1200

    1 file modified

--- src/app/course.ts
+++ src/app/course.ts
@@ -5,4 +5,12 @@ export class Course {
   period!: string;
   lecturer!: string;
   selectedCourse!: Course;
+
+  constructor() {
+    this.course_id = 1;
+    this.course_title = 'Web Development';
+    this.semester = 'One';
+    this.period = 'Wednesday 4-6pm';
+    this.lecturer = 'Dr. Jian Yu';
+  }
 }
